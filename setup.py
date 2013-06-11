cf06c8cf659df40827f1faebb6a740ff964c5885
Luke Orland <orluke@gmail.com>
2013-06-11 12:06:33 -0400

    minimum changes to turn into distributable package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Splitta',
+    version='1.07dev',
+    packages=['splitta',],
+    license='Apache License 2.0',
+    long_description=open('README').read(),
+)
